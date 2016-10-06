e5daf0e10ea6353667707ca457e1637cf2d25410
pelvark <pelvark@gmail.com>
2016-10-06 12:57:40 +0200

    added classes for skip lists

---
+++ skip-list.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+class Node(object):
+    def __init__(self, key):
+        #init
+        self.level
+        self.pointers = []
+
+
+class SkipList(object):
+    def __init__(self):
+        #init
+        self.head
+        self.tail
+        self.max_level
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    #handle input and run functions
