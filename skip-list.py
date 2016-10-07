a64e1f769a47fe5776eae9caee31d8688182fc4c
pelvark <pelvark@gmail.com>
2016-10-07 11:14:24 +0200

    added functions in skip-list

--- skip-list.py
+++ skip-list.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
+import random
 
 class Node(object):
     def __init__(self, key):
         #init
-        self.level
+        self.level = 0
         self.pointers = []
+
+
+    def determineLevel(self):
+        # detemine the highest possible level of a node
+        # should be changed to a formula with some probability.
+        return randint(1,10) 
 
 
 class SkipList(object):
@@ -14,6 +21,18 @@ class SkipList(object):
         self.tail
         self.max_level
 
+    
+    def insert(self, key)
+        # insert a node with key 
+        x = Node(key)
+
+
+    def delete(self, key):
+        # search for a node with key, and delete it
+
+
+    def search(self, key):
+        # search for a node with key
 
 
 
