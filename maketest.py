e31b65a71e03308338043f5fe665d83c913bc80d
pelvark <pelvark@gmail.com>
2016-10-26 02:21:48 +0200

    added a bigtest maker

--- maketest.py
+++ maketest.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import sys
 import random
 random.seed(42)
@@ -6,21 +7,21 @@ def randominput(n,f):
     l = list(range(0,n))
     random.shuffle(l)
     for i in l:
-        print("I ", i, file=f)
+        print("I", i, file=f)
     for i in range(0,n):
-        print("S ", random.randrange(0,n), file=f)
+        print("S", random.randrange(0,n), file=f)
 
 def randomwithdeletion(n,f):
     l = list(range(0,n))
     random.shuffle(l)
     for i in l:
-        print("I ", i, file=f)
+        print("I", i, file=f)
     for i in range(0,n):
         r = random.randint(0,1)
         if r == 1:
-            print("S ", random.randrange(0,n), file=f)
+            print("S", random.randrange(0,n), file=f)
         else:
-            print("D ", random.randrange(0,n), file=f)
+            print("D", random.randrange(0,n), file=f)
 
 def sortedinput(n,f):
     l = list(range(0,n))
@@ -30,18 +31,26 @@ def sortedinput(n,f):
     for i in l: 
         print("S ", i, file=f)
 
-def runtest1(n,f):
-    m = n%5
+def test1(n,f):
+    m = n%50
     n = n-m
     l = list(range(0,n))
     random.shuffle(l)
-    for i in range(0,n,5):
-        for k in range(0,5):
-            print("I ", l[i+k], file=f)
-        for k in range(0,5):
-            print("S ", l[random.randrange(0,i+5)], file=f)
+    for i in range(0,n,50):
+        for k in range(0,50):
+            print("I", l[i+k], file=f)
+        for k in range(0,50):
+            print("S", l[random.randrange(0,i+50)], file=f)
              
-        
+    
+def test2(n,f):
+    l = list(range(0,n))
+    random.shuffle(l)
+    for i in l:
+        print("I", i, file=f)
+    random.shuffle(l)
+    for i in l:
+        print("S", i, file=f)
 
 
 
@@ -60,9 +69,12 @@ if __name__=="__main__":
     elif sys.argv[1] == "sorted":
         f = open(sys.argv[3],"w")
         sortedinput(int(sys.argv[2]), f)
-    elif sys.argv[1] == "runtest1":
+    elif sys.argv[1] == "test1":
         f = open(sys.argv[3],"w")
-        runtest1(int(sys.argv[2]), f)
+        test1(int(sys.argv[2]), f)
+    elif sys.argv[1] == "test2":
+        f = open(sys.argv[3],"w")
+        test2(int(sys.argv[2]), f)
 
             
             
