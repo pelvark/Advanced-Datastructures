a03adcf766774cf4606322bacd7e742fefb6a55a
pelvark <pelvark@gmail.com>
2016-11-02 01:25:40 +0100

    initiated partially persistent doubly-linked lists

---
+++ PPDLL.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#Partially Persistent Doubly-Linked Lists
+import sys
+
+class Node(object):
+    def __init__(self, key):
+        self.key = key
+        self.next = None
+        self.prev = None
+
+    def newversion(self):
+        pass
+
+    def search(self, v, i):
+        pass
+
+    def insert(self, k, i):
+        pass
+
+    def update(self, k, i):
+        pass
+
+
+
+
+if __name__ == "__main__":
+    #handle input and run functions
+    pass
