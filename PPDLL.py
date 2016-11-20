5641482c603665a13b03f0bd61743bdb5b6969d4
pelvark <pelvark@gmail.com>
2016-11-20 16:41:55 +0100

    fixed header, added IO

--- PPDLL.py
+++ PPDLL.py
@@ -22,21 +22,29 @@ class LinkedList(object):
     def newversion(self):
         self.version += 1
 
+    # The operation search takes a version number v and an integer i as arguments and
+    # returns the key of the ith element of the vth version
     def search(self, v, i):
-        if len(self.header)>0:
-            # follow pointer with largest version smaller than v
+        j = len(self.header)
+        # follow pointer with largest version smaller than v
+        while j > 0:
+            if self.header[j][0] <= v:
+                x = self.header[j][1]
+                break
         else:
             return False
         while i>0:
             #find element in extra list with largest version smaller than v
             #if pointer is None, return false
             #follow pointer and decrement i
+            
 
     def insert(self, k, i):
         if i == 1:
+            
             # Then the element is placed first and a pointer is added to self.header
         elif len(self.header)>0:
-            x = self.header[-1]
+            x = self.header[-1][1]
         else: 
             return False
         while i>1:
@@ -68,4 +76,41 @@ class LinkedList(object):
 
 if __name__ == "__main__":
     #handle input and run functions
-    pass
+    ll = LinkedList()
+
+    while True:
+        line = sys.stdin.readline()
+        l = line.split()
+        if len(l)==0:
+            break
+        if l[0] == "I":
+            result = ll.insert(int(l[1]), int(l[2]))
+            if result:
+                print("S")
+                #print("S - comparisons used:", comparisoncounter, "rebuildsize:", rebuildsize)
+            else:
+                print("F")
+                #print("F - comparisons used:",comparisoncounter)
+        elif l[0] == "U":
+            result = tree.update(int(l[1]), int(l[2]))
+            if result:
+                print("S")
+                #print("S - comparisons used:",comparisoncounter)
+            else:
+                print("F")
+                #print("F - comparisons used:",comparisoncounter)
+        elif l[0] == "S":
+            result = ll.search(int(l[1]), int(l[2]))
+            if result is not None:
+                print("S")
+                #print("S - comparisons used:",comparisoncounter)
+            else:
+                print("F")
+                #print("F - comparisons used:",comparisoncounter)
+        elif l[0] == "N":
+            result = ll.newversion
+            print("S")
+        else:
+            break
+
+
